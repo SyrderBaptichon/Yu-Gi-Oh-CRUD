7e352858bec86be6d73e417e364ff131ad9207d7
Syrder Baptichon <dorybaptichon@gmail.com>
2024-03-21 17:00:26 +0100

    Entité edition et les fichiers relatives à cet entité

---
+++ src/Controller/EditionController.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Edition;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class EditionController extends AbstractController
+{
+    #[Route('/editions', name: 'app_edition')]
+    public function showAll(EntityManagerInterface $entityManager): Response
+    {
+        $editions = $entityManager->getRepository(Edition::class)->findAll();
+
+        if (!$editions) {
+            throw $this->createNotFoundException('Aucune édition trouvée !');
+        }
+
+        $tableRows = '';
+        foreach ($editions as $edition) {
+            $tableRows .= '
+            <tr>
+                <td>' . $edition->getNomEdition() . '</td>
+                <td>' . $edition->getNumEdition() . '</td>
+                <td>' . $edition->getDateEdition()->format('Y-m-d') . '</td>
+            </tr>
+        ';
+        }
+
+        $html = '
+        <h1>Voici les éditions :</h1>
+        <table border="1">
+            <thead>
+                <tr>
+                    <th>Nom de l\'édition</th>
+                    <th>Numéro de l\'édition</th>
+                    <th>Date de l\'édition</th>
+                </tr>
+            </thead>
+            <tbody>
+                ' . $tableRows . '
+            </tbody>
+        </table>
+    ';
+
+        return new Response($html);
+    }
+
+
+    #[Route('/edition/insert/', name: 'create_editon')]
+    public function createEdition(EntityManagerInterface $entityManager): Response
+    {
+        $edition = new Edition();
+        $edition->setNumEdition(50);
+        $edition->setNomEdition('aaa');
+        // $date = new DateTime();
+        // $edition->setDateEdition($date);
+
+        // tell Doctrine you want to (eventually) save the Product (no queries yet)
+        $entityManager->persist($edition);
+
+        // actually executes the queries (i.e. the INSERT query)
+        $entityManager->flush();
+
+        return new Response('Nouvelle édition insérer avec id= '.$edition->getId());
+    }
+
+    #[Route('/edition/{id}', name: 'show_editon')]
+    public function show(EntityManagerInterface $entityManager, int $id): Response
+    {
+        $edition = $entityManager->getRepository(Edition::class)->find($id);
+
+        if (!$edition) {
+            throw $this->createNotFoundException(
+                'No product found for id '.$id
+            );
+        }
+
+        return new Response('Voici l\'édition: '.$edition->getNomEdition());
+    }
+
+    #[Route('/edition/update/{id}', name: 'update_edition')]
+    public function update(EntityManagerInterface $entityManager, int $id): Response
+    {
+        $edition = $entityManager->getRepository(Edition::class)->find($id);
+
+        if (!$edition) {
+            throw $this->createNotFoundException(
+                'No product found for id '.$id
+            );
+        }
+
+        $edition->setNumEdition($edition->getNumEdition());
+        $edition->setNomEdition('aaa');
+        $entityManager->flush();
+
+        return new Response('Ce qui a été mise à jour : '.$edition->getNomEdition());
+
+    }
+
+    #[Route('/edition/delete/{id}', name: 'delete_edition')]
+    public function delete(EntityManagerInterface $entityManager, int $id): Response
+    {
+        $edition = $entityManager->getRepository(Edition::class)->find($id);
+
+        if (!$edition) {
+            throw $this->createNotFoundException(
+                'No product found for id '.$id
+            );
+        }
+
+        $edition->setNumEdition($edition->getNumEdition());
+        $edition->setNomEdition($edition->getNomEdition());
+        $entityManager->remove($edition);
+        $entityManager->flush();
+
+        return new Response('Ce qui a été supprimer : '.$edition->getNomEdition());
+    }
+}
---
+++ src/Entity/Edition.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EditionRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: EditionRepository::class)]
+class Edition
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $num_edition = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nom_edition = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $date_edition = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNumEdition(): ?int
+    {
+        return $this->num_edition;
+    }
+
+    public function setNumEdition(int $num_edition): static
+    {
+        $this->num_edition = $num_edition;
+
+        return $this;
+    }
+
+    public function getNomEdition(): ?string
+    {
+        return $this->nom_edition;
+    }
+
+    public function setNomEdition(string $nom_edition): static
+    {
+        $this->nom_edition = $nom_edition;
+
+        return $this;
+    }
+
+    public function getDateEdition(): ?\DateTimeInterface
+    {
+        return $this->date_edition;
+    }
+
+    public function setDateEdition(?\DateTimeInterface $date_edition): static
+    {
+        $this->date_edition = $date_edition;
+
+        return $this;
+    }
+}
