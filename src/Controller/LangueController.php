42076d57c20d57f8a8ef7e479d13e5c2ef625148
Amine <amine.cheikh@etu.univ-lehavre.fr>
2024-03-22 16:29:39 +0100

    validation d'un update et de l'insertion pour une langue faite !

--- src/Controller/LangueController.php
+++ src/Controller/LangueController.php
@@ -8,6 +8,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use PhpParser\Node\Scalar\String_;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -92,18 +93,48 @@ class LangueController extends AbstractController
             'id' => $product->getId()
         ]);
     }
-    #[Route("/{id}/dummy", name: 'modif_langue_form')]
+    #[Route("/{id}/update", name: 'modif_langue_form')]
 
-    public function formActionUpdate(EntityManagerInterface $entityManager,int $id)
+    public function formActionUpdate(EntityManagerInterface $entityManager,int $id, Request $request) :Response
     {
         $langue = $entityManager->getRepository(Langue::class)->find($id);
-        $dummyForm = $this->createForm(LangueType::class, $langue);
-        return $this->render('langue/formLangue.html.twig', array('form' => $dummyForm));
-    }
+        $form = $this->createForm(LangueType::class, $langue);
+        $form->handleRequest($request);
 
-    public function formActionInsert()
+        if ($form->isSubmitted() && $form->isValid()) {
+            $langue = $form->getData();
+            $entityManager->persist($langue);
+            // but, the original `$task` variable has also been updated
+            $entityManager->flush();
+            return $this->redirectToRoute('langues');
+        }
+
+        return $this->render('langue/formLangue.html.twig', array(
+            'form' => $form,
+        ));}
+    /**
+     *
+     * @Route("/insertLangueForm", name="musee_dummynew")
+     * @Method({"GET", "POST"})
+     */
+    #[Route("/formLangueInsert", name: "formulaire")]
+    public function action(EntityManagerInterface $entityManager, Request $request): Response
     {
+        $langue = new Langue();
+        $form = $this->createForm(LangueType::class, $langue);
+        $form->handleRequest($request);
 
+        if ($form->isSubmitted() && $form->isValid()) {
+            $langue = $form->getData();
+            $entityManager->persist($langue);
+            // but, the original `$task` variable has also been updated
+            $entityManager->flush();
+            return $this->redirectToRoute('langues');
+        }
+
+        return $this->render('langue/formLangue.html.twig', array(
+            'form' => $form,
+        ));
     }
     #[Route('/langue/delete/{id}', name: 'product_delete')]
 
