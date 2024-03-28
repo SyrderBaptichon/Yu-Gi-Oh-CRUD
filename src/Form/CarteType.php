5ebbf049aebdb89c0a65b9a5de195a90db0caf6d
ronald <awwal-ronald-oriade.fagbehouro@etu.univ-lehavre.fr>
2024-03-28 18:22:07 +0100

    Mise en forme du formulaire de modification d'une carte

--- src/Form/CarteType.php
+++ src/Form/CarteType.php
@@ -4,8 +4,10 @@ namespace App\Form;
 
 use App\Entity\Carte;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -17,16 +19,76 @@ class CarteType extends AbstractType
     {
         $builder
             ->add('carte_nom', TextType::class)
-            ->add('carte_attribut', TextType::class)
-            ->add('carte_categorie', TextType::class)
+            ->add('carte_attribut', ChoiceType::class, [
+                'choices' => [
+                    'Lumière' => 'lumière',
+                    'Feu' => 'feu',
+                    'Ténèbres' => 'ténèbres',
+                    'Vent' => 'vent',
+                    'Terre' => 'terre',
+                    'Eau' => 'eau',
+                ],
+            ])
+            ->add('carte_categorie', ChoiceType::class, [
+                'choices' => [
+                    'Monstre' => 'Monstre',
+                    'Magie' => 'Magie',
+                    'Piège' => 'Piège',
+                ],
+            ])
             ->add('carte_niveau', IntegerType::class)
-            ->add('carte_image', UrlType::class)
-            ->add('carte_type', TextType::class)
-            ->add('carte_specificite', TextType::class)
+            ->add('carte_image', URLType::class, [
+                'label' => 'Image',
+                'required' => false,
+            ])
+            ->add('carte_type', ChoiceType::class, [
+                'choices' => [
+                    'TONNERRE' => 'TONNERRE',
+                    'ELFE' => 'ELFE',
+                    'MACHINE' => 'MACHINE',
+                    'GUERRIER' => 'GUERRIER',
+                    'DRAGON' => 'DRAGON',
+                    'BÊTE' => 'BÊTE',
+                    'REPTILE' => 'REPTILE',
+                    'JEU RAPIDE' => 'JEU RAPIDE',
+                    'AQUA' => 'AQUA',
+                    'BÊTE-GUERRIER' => 'BÊTE-GUERRIER',
+                    'ROCHER' => 'ROCHER',
+                    'PYRO' => 'PYRO',
+                    'RITUEL' => 'RITUEL',
+                    'POISSON' => 'POISSON',
+                    'PLANTE' => 'PLANTE',
+                    'CONTRE' => 'CONTRE',
+                    'BÊTE AILÉE' => 'BÊTE AILÉE',
+                    'MAGICIEN' => 'MAGICIEN',
+                    'ÉQUIPEMENT' => 'ÉQUIPEMENT',
+                    'TERRAIN' => 'TERRAIN',
+                ],
+            ])
+            ->add('carte_specificite', ChoiceType::class, [
+                'choices' => [
+                    'FUSION' => 'FUSION',
+                    'UNION' => 'UNION',
+                    'AUCUNE' => 'AUCUNE',
+                    'PENDULE' => 'PENDULE',
+                    'XYZ' => 'XYZ',
+                    'SYNCHRO' => 'SYNCHRO',
+                    'RITUEL' => 'RITUEL',
+                    'TOON' => 'TOON',
+                    'LIEN' => 'LIEN',
+                    'SYNTONISEUR' => 'SYNTONISEUR',
+                    'GÉMEAU' => 'GÉMEAU',
+                    'FLIP' => 'FLIP',
+                    'EFFET' => 'EFFET',
+                ],
+            ])
             ->add('carteATK', IntegerType::class)
             ->add('carteDEF', IntegerType::class)
-            ->add('carte_description', TextType::class)
-            ->add('save', SubmitType::class)
+            ->add('carte_description', TextareaType::class) // Utilisation de TextareaType pour carte_description
+            ->add('save', SubmitType::class, [
+                'label' => 'Enregistrer', // Modifier le label du bouton de sauvegarde
+                'attr' => ['class' => 'button is-primary'] // Classes CSS pour le bouton
+            ]);
         ;
     }
 
